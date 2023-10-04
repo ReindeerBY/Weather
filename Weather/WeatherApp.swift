c1ed303e5b1ee2654139a3fbc577952f7c67ef4c
Eugene <zhekichman@gmail.com>
2023-10-04 15:55:12 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Admin on 4.10.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
