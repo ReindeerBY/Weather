7579adbc28817c8b32cf8b09225f8d84f54cf180
Eugene <zhekichman@gmail.com>
2023-10-04 16:49:08 +0300

    weather manager, location manager

---
+++ Weather/Managers/LocationManager/LocationManager.swift
@@ -0,0 +1,36 @@
+//
+//  LocationManager.swift
+//  Weather
+//
+//  Created by Admin on 4.10.23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Eror getting location", error)
+        isLoading = false
+    }
+}
---
+++ Weather/Managers/LocationManager/WeatherManager.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherManager.swift
+//  Weather
+//
+//  Created by Admin on 4.10.23.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=2b032ab6a8431d29a5948ab38139d145&units=metric") else {
+            fatalError("Missing URL")
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            fatalError("Error fetching weather data")
+        }
+        
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
+    }
+}
